76496ea5dd9ab5e4b47211a8faeda7497b2ba260
Oly Rajak <59551153+olyrajak@users.noreply.github.com>
2023-07-07 01:55:06 +0530

    Update app.js

--- authusingpassport/app.js
+++ authusingpassport/app.js
@@ -10,8 +10,11 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
